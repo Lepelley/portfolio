a7cdc7125389c2d91929d3f113f616e34d99cafa
Vincent Lepelley <v.lepelley@gmail.com>
2019-09-16 18:07:40 +0200

    Initial commit

---
+++ blog.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="description" content="Portfolio de Vincent Lepelley, développeur web HTML 5 CSS 3 JavaScript PHP SQL">
+    <meta name="keywords" content="Vincent, Lepelley, web, developer, développeur, HTML, HTML5, CSS, CSS3, JavaScript, PHP, SQL">
+    <meta name="author" content="Vincent Lepelley">  
+    <link rel="author" href="http://vincent.lepelley.fr">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Vincent Lepelley ~ Développeur Web &raquo; Réalisations</title>
+</head>
+<body>
+    <header id="header">
+        <h1>Vincent Lepelley ~ Développeur Web &raquo; Blog</h1>
+        <!--<?php echo htmlspecialchars('<h1>Vincent Lepelley<br />Développeur web</h1>'); ?>-->
+
+        <nav>
+            <ul class="list-no-style">
+                <li><a href="index.html">Réalisations</a></li>
+                <li class="active">Blog</li>
+                <li><a href="cv.html">Curriculum vitae</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="container">
+        <section id="blog">
+           <article>
+               <h1>Mon avis sur Bootstrap</h1>
+               <p>
+                   <img src="https://media.licdn.com/dms/image/C5612AQFaSozKVXUUZQ/article-cover_image-shrink_720_1280/0?e=1573689600&v=beta&t=M4DtYjhyLYVczUu8Fy7UxykzBuWm19TAnE3L7d-2YeM" alt="Logo Bootstrap">
+                    Cet article est un repost de l'article que j'ai posté sur LinkedIn (<a href="#">lien</a>). Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore!
+                </p>
+                
+                <p id="read-more"><a href="article.php">Lire la suite de l'article</a></p>
+
+           </article>
+           <aside>
+               <p>
+                    Catégories : Bootstrap, CSS, Développement Web<br>
+                    Publié par Vincent le 9 septembre 2019<br>
+                    Dernière mise à jour le 10 septembre 2019
+               </p>
+           </aside>
+
+           <article>
+               <h1>Titre de l'article</h1>
+               <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit facere voluptate culpa doloremque fugit tempore eius, error, alias dicta delectus voluptates perferendis amet! At aut
+                    voluptas harum aliquid molestias tempore!
+
+                    <span id="read-more"><a href="article.php">Lire la suite de l'article</a></span>
+
+               </p>
+           </article>
+           <aside>
+               <p>
+                    Catégories : Bootstrap, CSS, Développement Web<br>
+                    Publié par Vincent le 9 septembre 2019<br>
+                    Dernière mise à jour le 10 septembre 2019
+               </p>
+           </aside>
+
+           <p>Page 1 - <a href="#">2</a></p>
+        </section>
+    </div>
+</body>
+</html>
