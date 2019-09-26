940446dbea51af3c7065505299e117006dc6808d
Vincent Lepelley <v.lepelley@gmail.com>
2019-09-26 05:24:08 +0200

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -13,6 +13,8 @@ class Manager
     protected function dbConnect()
     {
         $db = new \PDO($this->dns . ':host=' . $this->host . ';dbname=' . $this->db . ';charset=utf8', $this->user, $this->password);
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
         return $db;
     }
 }
